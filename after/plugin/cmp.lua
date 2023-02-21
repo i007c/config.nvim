c058c66a80c030c8df401bedd3585d69636ee579
007 <dr007cc@gmail.com>
2023-02-21 21:33:06 +0330

    fix: add lsp config and debug cmp

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -12,7 +12,8 @@ require('luasnip.loaders.from_vscode').lazy_load()
 
 
 local has_words_before = function()
-    local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
 
@@ -33,7 +34,8 @@ cmp.setup({
     mapping = {
         ['<Up>'] = cmp.mapping.select_prev_item(),
         ['<Down>'] = cmp.mapping.select_next_item(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        -- ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-e>'] = cmp.mapping.abort(),
         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i' }),
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,18 +5,53 @@ local lsp = require('lsp-zero')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local mlspc = require('mason-lspconfig')
 
-
 lsp.preset({
     name = 'recommended',
     manage_nvim_cmp = false,
-    configure_diagnostics = false
+    configure_diagnostics = false,
+    set_lsp_keymaps = true,
 })
 
 lsp.nvim_workspace()
 lsp.setup()
 
 
+local function lsp_keymaps(bufnr)
+    local map = function(m, lhs, rhs)
+        local opts = {remap = false, silent = true, buffer = bufnr}
+        vim.keymap.set(m, lhs, rhs, opts)
+    end
+
+    -- LSP actions
+    map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+    map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+    map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+    map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+    map('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+    map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+    map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+    map('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+    map('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+    map('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
+
+    -- Diagnostics
+    map('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+    map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+    map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+end
+
+local function lsp_attach(client, bufnr)
+    local buf_command = vim.api.nvim_buf_create_user_command
+
+    lsp_keymaps(bufnr)
+
+    buf_command(bufnr, 'LspFormat', function()
+        vim.lsp.buf.format()
+    end, {desc = 'Format buffer with language server'})
+end
+
 lspconfig.ccls.setup({
+    on_attach = lsp_attach,
     init_options = {
         cache = {
             directory = '.ccls-cache';
@@ -30,12 +65,14 @@ lspconfig.ccls.setup({
 mlspc.setup_handlers({
     function (server_name)
         lspconfig[server_name].setup({
+            on_attach = lsp_attach,
             capabilities = capabilities
         })
     end,
     lua_ls = function ()
         lspconfig.lua_ls.setup({
             capabilities = capabilities,
+            on_attach = lsp_attach,
             settings = {
                 Lua = {
                     diagnostics = {
