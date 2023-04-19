668bb1688e5d71e2f057d326a77b7cb3076b9eac
007 <dr007cc@gmail.com>
2023-04-19 13:11:55 +0330

    fix: update remaps

--- after/plugin/remap.lua
+++ after/plugin/remap.lua
@@ -20,3 +20,14 @@ vim.keymap.set({'n', 'i'}, '<C-S>', vim.cmd.w)
 
 vim.keymap.set('n', '<S-B>', '<CMD>LspUI diagnostic next<CR>')
 
+
+-- now this is epic - ThePrimeagen
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+
