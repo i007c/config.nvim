19cb5408f89e8fe9bfd18ba81515f5f48024a060
007 <dr007cc@gmail.com>
2023-05-15 09:44:53 +0330

    fix: diffrent autocmds for .py and .c stuff

--- lua/i007c/runner.lua
+++ lua/i007c/runner.lua
@@ -10,7 +10,12 @@ vim.api.nvim_create_user_command('RunTyche', (function ()
 
         vim.api.nvim_clear_autocmds({
             event = 'BufWritePost',
-            group = 'Tyche'
+            group = 'TycheC'
+        })
+
+        vim.api.nvim_clear_autocmds({
+            event = 'BufWritePost',
+            group = 'TychePy'
         })
     end
 
@@ -28,22 +33,38 @@ vim.api.nvim_create_user_command('RunTyche', (function ()
 
     vim.cmd('wincmd h')
 
+    local write_lines = function(_, data)
+        vim.api.nvim_buf_set_lines(BufNr, -1, -1, false, data)
+    end
+
     vim.api.nvim_create_autocmd('BufWritePost', {
-        group = vim.api.nvim_create_augroup('Tyche', { clear = true }),
-        pattern = '*/plutus/tyche/*.{c,py,h}',
+        group = vim.api.nvim_create_augroup('TycheC', { clear = true }),
+        pattern = '*/plutus/tyche/*.{c,h}',
         callback = function()
-            vim.api.nvim_buf_set_lines(BufNr, 0, -1, false, {'output:', ''})
+            vim.api.nvim_buf_set_lines(BufNr, 0, -1, false, {'build:', ''})
 
             vim.fn.jobstart({'./tyche/build.sh'}, {
                 stdout_buffered = true,
                 stderr_buffered = true,
-                on_stdout = function(_, data)
-                    vim.api.nvim_buf_set_lines(BufNr, -1, -1, false, data)
-                end,
-                on_stderr = function(_, data)
-                    vim.api.nvim_buf_set_lines(BufNr, -1, -1, false, data)
-                end
+                on_stdout = write_lines,
+                on_stderr = write_lines,
             })
         end,
     })
+
+    vim.api.nvim_create_autocmd('BufWritePost', {
+        group = vim.api.nvim_create_augroup('TychePy', { clear = true }),
+        pattern = '*/plutus/tyche/*.py',
+        callback = function()
+            vim.api.nvim_buf_set_lines(BufNr, 0, -1, false, {'run:', ''})
+
+            vim.fn.jobstart({'python', 'tyche/test.py'}, {
+                stdout_buffered = true,
+                stderr_buffered = true,
+                on_stdout = write_lines,
+                on_stderr = write_lines,
+            })
+        end,
+    })
+
 end), {})
