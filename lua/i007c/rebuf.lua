b63da894de8b12800a5cbb7c08767a33be2f2357
007 <dr007cc@gmail.com>
2024-02-05 11:41:34 +0330

    fix: update rebuf

--- lua/i007c/rebuf.lua
+++ lua/i007c/rebuf.lua
@@ -8,4 +8,13 @@ vim.api.nvim_create_user_command('Rebuf', (function(ctx)
     vim.cmd('set filetype=text')
     vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
     vim.opt_local.modified = false
+
+    local bufnr = vim.api.nvim_win_get_buf(0)
+
+    vim.keymap.set(
+        'n', 'q',
+        function() vim.api.nvim_buf_delete(bufnr, { force = true }) end,
+        { buffer = bufnr, noremap = true, silent = true, nowait = true }
+    )
+
 end), { nargs = '+', complete = 'command' })
