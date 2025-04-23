5139971be7c4f2cb21ac8207a2e1af7c59529bb1
007 <dr007cc@gmail.com>
2025-04-24 02:27:48 +0330

    fix: update nvim tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -100,6 +100,7 @@ return {
                 },
                 exclude = {
                     'build.gradle',
+                    'build.sh',
                 }
             },
             renderer = {
