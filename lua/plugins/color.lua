69190eec8dc8248553ccef6502e1f6809a699117
007 <dr007cc@gmail.com>
2025-04-24 22:33:52 +0330

    fix: update colors

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -40,6 +40,7 @@ local group = {
     ['@constant'] = { fg = '#f2f4f8' },
     ['@lsp.type.const.rust'] = { link = '@constant' },
     ['@lsp.type.procMacro.rust'] = { link = 'PreProc' },
+    ['@function.macro'] = { link = 'PreProc' },
 
     ['@tag.scss'] = { fg = '#ffcb7f' },
     ['@tag.tsx'] = { fg = '#ffcb7f' },
