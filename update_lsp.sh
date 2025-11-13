c57475707653bef8c19bc0d55ef3c939b684eb36
007 <dr007cc@gmail.com>
2025-11-13 18:27:41 +0330

    feat: glsl_analyzer

--- update_lsp.sh
+++ update_lsp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for i in {ts_ls,rust_analyzer,lemminx,jsonls,pylsp,lua_ls,taplo,cssls,dartls,kotlin_language_server}; do
+for i in {ts_ls,rust_analyzer,lemminx,jsonls,pylsp,lua_ls,taplo,cssls,dartls,kotlin_language_server,glsl_analyzer}; do
     cp -f nvim-lspconfig/lsp/$i.lua after/lsp/
 done
